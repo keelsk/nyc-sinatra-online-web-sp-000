5e58679e367e944234950025a8c2173ad1656169
Kirsten Keels <keelsk@gmail.com>
2020-04-04 17:28:21 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -19,7 +19,7 @@ class LandmarksController < ApplicationController
     erb :'landmarks/show'
   end
 
-  get 'landmarks/:id/edit' do
+  get '/landmarks/:id/edit' do
     @landmark = Landmark.find_by_id(params[:id])
     erb :'landmarks/edit'
   end
