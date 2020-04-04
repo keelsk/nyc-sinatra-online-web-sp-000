93d3a9439a3ac5d2cf67d04ec77141b7d93ef8ee
Kirsten Keels <keelsk@gmail.com>
2020-04-04 15:21:21 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -3,4 +3,9 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do
     erb :'landmarks/new'
   end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
 end
